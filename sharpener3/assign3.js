40c2268cb6636178b4097224925e438cc6af2f12
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-03-16 22:20:12 +0530

    querySelector and querySelectorAll

---
+++ sharpener3/assign3.js
@@ -0,0 +1,7 @@
+let secondItem=document.querySelector('.list-group-item:nth-child(2)');  
+console.log(secondItem); 
+secondItem.style.backgroundColor="green"
+
+let thirdItem=document.querySelector('.list-group-item:nth-child(3)');  
+console.log(thirdItem); 
+thirdItem.style.color="transparent"
