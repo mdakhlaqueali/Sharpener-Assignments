f20fd1ebd250445d88037417ca3ac81e60dfa911
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-03-25 15:44:49 +0530

    storing more data in array

---
+++ 9-Lets Scale the app to more users/ind.js
@@ -0,0 +1,49 @@
+// USER FORM SCRIPT
+
+// Put DOM elements into variables
+let myForm = document.querySelector('#my-form');
+let nameInput = document.querySelector('#name');
+let emailInput = document.querySelector('#email');
+let msg = document.querySelector('.msg');
+let userList = document.querySelector('#users');
+
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  if(nameInput.value === '' || emailInput.value === '') {
+    // alert('Please enter all fields');
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    // Remove error after 3 seconds
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    // Create new list item with user
+    const li = document.createElement('li');
+
+    // Add text node with input values
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+
+    // Add HTML
+    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+
+    // Append to ul
+    userList.appendChild(li);
+
+    //setting in localStorage
+    let myObj = JSON.parse(localStorage.getItem('users')) || [];
+    myObj.push({
+        name: nameInput.value,
+        email: emailInput.value
+    })
+    myObjJSON= JSON.stringify(myObj)
+    localStorage.setItem("users", myObjJSON)
+
+    // Clear fields
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
