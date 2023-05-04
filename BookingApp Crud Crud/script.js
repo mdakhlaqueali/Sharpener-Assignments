1d0fb4bc29789e1ba2461fd00e8828a7ce13f7bf
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-05-04 18:47:55 +0530

    delete functionality added

--- BookingApp Crud Crud/script.js
+++ BookingApp Crud Crud/script.js
@@ -43,7 +43,37 @@ function display(res){
     let emailCell = document.createElement("td");
     emailCell.textContent = el.email;
 
-    newRow.append(nameCell, emailCell);
+    let deleteCell = document.createElement('td');
+        
+    let editCell = document.createElement('td');
+        
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", deleteUser);
+    deleteCell.append(deleteButton);
+        
+        
+    let editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    // editButton.addEventListener("click", editUser);
+    editCell.append(editButton);
+
+    newRow.append(nameCell, emailCell, deleteCell, editCell);
+    newRow.setAttribute("data-id", el._id);//set the unique id from api to the row as attribute
     parent.appendChild(newRow);
     });
+}
+function deleteUser(event) {
+    let row = event.target.parentNode.parentNode;
+    let userId = row.getAttribute("data-id");
+
+    axios.delete(`https://crudcrud.com/api/3b2934afab514f36824a66b7135edb7a/myData/${userId}`)
+        .then(function(response) {
+            row.remove();
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
 }
