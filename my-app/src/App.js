38b7848705d6ab41fb56e2ece8b0ec2366df7ec0
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-09-04 21:53:03 +0530

    Passing data via props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,24 @@
 import ExpenseItem from "./components/ExepenseItem";
 function App() {
+  const expenses = [
+    {title: "Car Insurance", amount: 287, date: new Date(2021, 2 , 22)},
+    {title: "Medicine", amount: 1257, date: new Date(2021, 7 , 22)},
+    {title: "Fuel", amount: 1987, date: new Date(2021, 5 , 22)},
+    {title: "Electricity", amount: 2187, date: new Date(2021, 9 , 22)},
+    {title: "Grocery", amount: 787, date: new Date(2021, 10 , 22)}
+
+  ]
   return (
     <div className="App">
       <h2>Let's get started</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+      
     </div>
   );
 }
--- my-app/src/components/ExepenseItem.js
+++ my-app/src/components/ExepenseItem.js
@@ -1,16 +1,11 @@
 import './ExpenseItem.css';
-function ExpenseItem(){
-    const ExpenseDate = new Date(2021, 2, 28);
-    const ExpenseTitle = "Car Insurance";
-    const ExpenseAmount = 294.3;
-    const LocationOfExpenditure = "Patna";
+function ExpenseItem(props){
     return(
         <div className="expense-item">
-            <div>{ExpenseDate.toDateString()}</div>
+            <div>{props.date.toDateString()}</div>
             <div className="expense-item__description">
-                <h2>{ExpenseTitle}</h2>
-                <h2>{LocationOfExpenditure}</h2>
-                <div className="expense-item__price">{ExpenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     );
