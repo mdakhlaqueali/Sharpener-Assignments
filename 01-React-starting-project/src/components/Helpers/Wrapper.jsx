d9b6b1d9f09b58094cc2db84777a1f42cb6505eb
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-10-19 01:23:10 +0530

    Using React Fragments

--- 01-React-starting-project/src/components/Helpers/Wrapper.jsx
+++
@@ -1,4 +0,0 @@
-const Wrapper = (props) =>{
-    return props.children;
-}
-export default Wrapper;
--- 01-React-starting-project/src/components/Users/AddUser.jsx
+++ 01-React-starting-project/src/components/Users/AddUser.jsx
@@ -3,7 +3,6 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
-import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) =>{
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -45,7 +44,7 @@ const AddUser = (props) =>{
     };
 
     return(
-        <Wrapper>
+        <>
         {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
@@ -56,7 +55,7 @@ const AddUser = (props) =>{
             <Button type="submit">Add User</Button>
         </form>
         </Card>
-        </Wrapper>
+        </>
     )
 };
 
