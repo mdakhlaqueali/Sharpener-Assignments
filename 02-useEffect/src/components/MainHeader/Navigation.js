9ca89133e1b6dc2cd8e3c3a4cfb78c8c96231013
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-10-24 23:41:23 +0530

    contextAPI

--- 02-useEffect/src/components/MainHeader/Navigation.js
+++ 02-useEffect/src/components/MainHeader/Navigation.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from '../store/auth-context';
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
   return (
-    <nav className={classes.nav}>
+          <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
