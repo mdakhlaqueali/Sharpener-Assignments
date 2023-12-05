49e5614c1de64a9b307752e5d3bddad487134dab
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-12-05 17:57:04 +0530

    Building the header of your restaurant app

---
+++ creating-restaurant-website/src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+    </Fragment>
+  );
+}
+
+export default App;
