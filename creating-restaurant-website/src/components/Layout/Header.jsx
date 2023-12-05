012e95e2525ebd018ff8a27b51d5c7b401fc7792
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-12-05 22:53:23 +0530

    Cart open and close

--- creating-restaurant-website/src/components/Layout/Header.jsx
+++ creating-restaurant-website/src/components/Layout/Header.jsx
@@ -3,15 +3,16 @@ import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 import classes from './Header.module.css';
 
-const Header = props =>{
-    return <Fragment>
+const Header = (props) =>{
+    return( <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton/>
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
         <div className={classes["main-image"]}>
             <img src={mealsImage} alt="A table full of delicious foods"/>
         </div>
     </Fragment>
+    )
 }
 export default Header;
