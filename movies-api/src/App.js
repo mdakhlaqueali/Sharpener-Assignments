b71195088f579bf1c20d164b387487bce5074cf1
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-12-14 18:15:06 +0530

    Fetch Movie on Page load

--- movies-api/src/App.js
+++ movies-api/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler(){
+  const fetchMoviesHandler = useCallback(async()=>{
   setIsLoading(true);
   setError(null);
   try{
@@ -33,7 +33,11 @@ function App() {
     setError(error.message);
   }
   setIsLoading(false);
-  }
+  }, []);
+  
+  useEffect(()=>{
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
