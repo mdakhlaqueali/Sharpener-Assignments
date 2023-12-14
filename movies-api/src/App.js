37eb7e1db1c2cb41f6f46b086e04e5a893c23b28
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-12-14 17:36:23 +0530

    Loading the application

--- movies-api/src/App.js
+++ movies-api/src/App.js
@@ -5,37 +5,24 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler(){
-    fetch('https://swapi.dev/api/films')
-    .then((response)=>{
-      return response.json();
-    })
-    .then((data)=>{
-      const transformedMovies = data.results.map(movieData=>{
-        return{
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
-      setMovies(transformedMovies);
-    })
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchMoviesHandler(){
+  setIsLoading(true);
+  const response = await fetch('https://swapi.dev/api/films')
+  const data = await response.json();
+    
+    const transformedMovies = data.results.map(movieData=>{
+      return{
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
   }
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
 
   return (
     <React.Fragment>
@@ -43,7 +30,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length>0 && <MoviesList movies={movies}/>}
+        {!isLoading && movies.length===0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
