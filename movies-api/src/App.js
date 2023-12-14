1b8b88aee9f822e5daad4a665224c168e7fce3c9
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-12-14 15:34:30 +0530

    Making Api calls in React

---
+++ movies-api/src/App.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  function fetchMoviesHandler(){
+    fetch('https://swapi.dev/api/films')
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      const transformedMovies = data.results.map(movieData=>{
+        return{
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      });
+      setMovies(transformedMovies);
+    })
+  }
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
