576c069116aa70f826e5fc63e67b5b9ea5da03f2
mdakhlaqueali <mdakhlaqueali@gmail.com>
2023-12-15 21:25:49 +0530

    Updating the Old Password

--- authentication-project/src/components/Profile/ProfileForm.js
+++ authentication-project/src/components/Profile/ProfileForm.js
@@ -1,9 +1,12 @@
 import { useRef, useContext } from 'react';
-
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+
+  const history = useHistory();
+
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -26,8 +29,7 @@ const ProfileForm = () => {
       }
     }).then(res => {
       // assumption: Always succeeds!
-
-
+      history.replace('/');
     });
   };
 
