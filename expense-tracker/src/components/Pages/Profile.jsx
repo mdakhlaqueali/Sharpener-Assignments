ed31299f81c8d7f1495cf1652cd30ad3965dd829
mdakhlaqueali <mdakhlaqueali@gmail.com>
2024-01-13 20:42:29 +0530

    Edit User Details

--- expense-tracker/src/components/Pages/Profile.jsx
+++ expense-tracker/src/components/Pages/Profile.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
     const displayNameRef = useRef();
     const photoUrlRef = useRef();
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState({});
   
     const updateProfileHandler = async (event) => {
       event.preventDefault();
@@ -24,6 +27,13 @@ const Profile = () => {
         };
   
         const response = await axios.post(url, updateData);
+
+        if (response.status === 200) {
+            navigate('/')
+          } else {
+            // Handle error responses (e.g., invalid token)
+            console.error("Profile update failed:", response.data.error);
+          }
   
         // The response should contain user data or an error message
         console.log(response.data);
@@ -32,7 +42,35 @@ const Profile = () => {
         console.error('Profile update failed:', error);
         // Handle update failure, show an error message, etc.
       }
-    }
+    };
+
+    const fetchUserData = async () => {
+        const idToken = localStorage.getItem('token');
+  
+        if (idToken) {
+          const url = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBiEoVyC6pTrqMH33q1rNiN1-Ck8F40X8M`;
+  
+          try {
+            const response = await axios.post(url, { idToken });
+            if (response.status === 200) {
+              const user = response.data.users[0];
+              setUserData(user);
+              displayNameRef.current.value = user.displayName || '';
+              photoUrlRef.current.value = user.photoUrl || '';
+            } else {
+              // Handle error responses
+              console.error("Failed to fetch user data:", response.data.error);
+            }
+          } catch (error) {
+            console.error('Failed to fetch user data:', error);
+          }
+        }
+      };
+
+      useEffect(()=>{
+        fetchUserData();
+      }, []);
+
   return (
     <form onSubmit={updateProfileHandler}>
       <h2>Update Profile</h2>
