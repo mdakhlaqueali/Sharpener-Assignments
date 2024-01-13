8646b09447158b4df6fc52cbc4131f596d910346
mdakhlaqueali <mdakhlaqueali@gmail.com>
2024-01-13 23:38:49 +0530

    Integrating it with firebase

--- expense-tracker/src/components/Pages/Expenses/Expenses.jsx
+++ expense-tracker/src/components/Pages/Expenses/Expenses.jsx
@@ -1,27 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ExpenseDetails from "./ExpenseDetails";
 import styles from "./Expenses.module.css";
+import axios from "axios";
 
 const Expenses = () => {
   const [expense, setExpense] = useState([]);
   const [amount, setAmount] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("Grocery");
-  const addExpenesHandler = (event) => {
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
+  const addExpenesHandler = async (event) => {
     event.preventDefault();
 
     const amountData = {
-      id: Math.random().toString(),
       amount: +amount,
       description,
       category,
     };
 
-    setExpense((prevAmount) => [...prevAmount, amountData]);
+    // setExpense((prevAmount) => [...prevAmount, amountData]);
+    const url =
+      "https://expense-tracker-20439-default-rtdb.firebaseio.com/expenses.json";
 
+    try {
+      await axios.post(url, amountData);
+      fetchExpenses();
+    } catch (error) {
+      console.log(error);
+    }
     setAmount("");
     setCategory("Grocery");
     setDescription("");
+  };
+
+  const fetchExpenses = async () => {
+    const url =
+      "https://expense-tracker-20439-default-rtdb.firebaseio.com/expenses.json";
+    try {
+      const response = await axios.get(url);
+      if (response.status === 200) {
+        const expenseData = response.data;
+
+        const expenseArray = Object.keys(expenseData).map((key) => ({
+          id: key,
+          ...expenseData[key],
+        }));
+
+        console.log("expenseArray", expenseArray);
+        console.log("response-data", response.data);
+        setExpense(expenseArray);
+      } else {
+        console.log("failed to fetch");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
