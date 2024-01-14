a2f639497d460e6c4495d70d07658541a6e9af16
mdakhlaqueali <mdakhlaqueali@gmail.com>
2024-01-14 14:46:13 +0530

    Subscribing and Dispatching Actions

--- redux/starting-project/src/components/Counter.js
+++ redux/starting-project/src/components/Counter.js
@@ -20,8 +20,8 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementHandler}>Increment By 5</button>
+        <button onClick={decrementHandler}>Decrement By 5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- redux/starting-project/src/store/index.js
+++ redux/starting-project/src/store/index.js
@@ -3,12 +3,12 @@ import {createStore} from "redux";
 const counterReducer = (state={counter:0}, action) => {
     if(action.type === 'increment'){
         return{
-            counter: state.counter + 1
+            counter: state.counter + 5
         }
     }
     if(action.type === 'decrement'){
         return{
-            counter: state.counter - 1
+            counter: state.counter - 5
         }
     }
     return state;
