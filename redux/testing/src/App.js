b0300af6b8719ecfb99fc64a7ce761ba3dc982dc
mdakhlaqueali <mdakhlaqueali@gmail.com>
2024-01-17 01:33:09 +0530

    TDD Async code

--- redux/testing/src/App.js
+++ redux/testing/src/App.js
@@ -1,9 +1,11 @@
 import Greeting from './components/Greeting';
+import Async from './components/Async';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Async/>
       <Greeting/>
     </div>
   );
