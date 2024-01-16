3632c2f054c3f6fabb8ca28910b796dd314949ac
mdakhlaqueali <mdakhlaqueali@gmail.com>
2024-01-16 22:32:00 +0530

    Test Driven Development (TDD)

---
+++ redux/testing/src/components/Greeting.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+
+describe('Greeting component', ()=>{
+    test('renders Hello world as a text', ()=>{
+        //Arrange
+        render(<Greeting/>);
+    
+        //Act
+        //...nothing
+    
+        //Assert
+        const helloWorldElement = screen.getByText('Hello World');
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+});
